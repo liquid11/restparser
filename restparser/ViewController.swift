7776660b9f0da4400edd13b7c9cc01b02bcd7087
liquid11 <orangevolution@yahoo.com>
2015-06-15 20:06:09 +0500

    Initial Commit

---
+++ restparser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  restparser
+//
+//  Created by Tezro Solutions on 15/06/2015.
+//  Copyright (c) 2015 Tezro Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
